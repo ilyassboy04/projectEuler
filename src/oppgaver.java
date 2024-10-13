322a6d63e3829d15d632eafda75f23c019ce90da
ilyassboy04 <elsah1344@oslomet.no>
2024-10-13 16:53:50 +0200

    løste oppgave 2

--- src/oppgaver.java
+++ src/oppgaver.java
@@ -11,6 +11,19 @@ public class oppgaver {
     }
 
     public static void oppgave2(){ //sum of even numbers in fib under 4*10^6
-
+        int n1 = 1;
+        int n2= 2;
+        int n3 = 0;
+        int sum = 2;
+        while (sum < 4_000_000){
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            System.out.println(n3);
+            if(n3%2==0) {
+                sum += n3;
+            }
+        }
+        System.out.println(sum);
     }
 }
